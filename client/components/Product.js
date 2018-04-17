20889ac7304c2b14c44b43b7b7ad322e3c67d456
Jane Costa <yberdysh@gmail.com>
2018-04-17 16:43:41 -0400

    formatting issues

--- client/components/Product.js
+++ client/components/Product.js
@@ -7,16 +7,13 @@ class Product extends Component {
       const product = this.props.product
         return (
             <div>
-                {
-                    product &&
-                        <div key={product.id}>
-                        <h1> {product.name} </h1>
-                        <img class='media-object' src={product.picture}/>
-                        <h2> Metaphysical Properties </h2>
-                        <p> {product.properties} </p>
-
-                        </div>
-                }
+                {product &&
+                  <div>
+                    <h1> {product.name} </h1>
+                    <img class='media-object' src={product.picture}/>
+                    <h2> Metaphysical Properties </h2>
+                    <p> {product.properties} </p>
+                  </div>}
             </div>
         );
     }
