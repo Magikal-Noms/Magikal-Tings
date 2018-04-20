18095ca3a57c4309b69f3b4603aa230e4a8096c1
Jane Costa <yberdysh@gmail.com>
2018-04-20 12:30:25 -0400

    Categories routes working

--- server/api/categories.js
+++ server/api/categories.js
@@ -4,10 +4,21 @@ const {Category, Product} = require('../db/models')
 
 module.exports = router;
 
+
+router.get('/', (req, res, next) => {
+ Category.findAll({
+ })
+ .then(categories => res.json(categories))
+ .catch(next)
+})
+
+
 router.get('/:categoryId/products', (req, res, next) => {
  Product.findAll({
    where: {
      categoryId : +req.params.categoryId
    }
  })
+ .then(products => res.json(products))
+ .catch(next)
 })
