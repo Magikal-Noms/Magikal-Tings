db7c80edd17f93e497606374353b20e67500713e
Jane Costa <yberdysh@gmail.com>
2018-04-20 14:50:17 -0400

    productCard

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -2,31 +2,18 @@ import { Link } from "react-router-dom";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Sidebar } from "./Sidebar";
+import {ProductCard} from './ProductCard'
 
 class AllProducts extends Component {
   render() {
     let products = this.props.products;
-    let categories = this.props.categories
+    let categories = this.props.categories;
     return (
       <div>
-        <Sidebar categories={this.props.categories} />
+        <Sidebar product={this.props.product} />
         {products.map(product => {
-          return (
-            <div key={product.id}>
-              <h1 className="product-title"> {product.name} </h1>
-              <img className="media-object" src={product.picture} />
-              <h2 className="product-price"> Price: {product.price} </h2>
-              <Link to={`/products/${product.id}`}>
-                {" "}
-                <button> {product.name} </button>
-              </Link>
-            </div>
-          );
+          return (<ProductCard product={product}/>);
         })}
-
-
-
-
       </div>
     );
   }
@@ -35,9 +22,11 @@ class AllProducts extends Component {
 const mapStateToProps = state => {
   return {
     products: state.products,
+    product: state.products.find(ele => {
+      return ele.id === +ownProps.match.params.productId;
+    }),
     categories: state.categories
   };
 };
 
 export default connect(mapStateToProps)(AllProducts);
-
