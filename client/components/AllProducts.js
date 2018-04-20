1607e6b4df5963d6ffc8765a2b84d5676d3bd5f2
Priya <banspri0@gmail.com>
2018-04-20 11:23:59 -0400

    Rendering stuff in localhost:8080/cart

---
+++ client/components/AllProducts.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Sidebar } from "./Sidebar";
+
+class AllProducts extends Component {
+  render() {
+    let products = this.props.products;
+    return (
+      <div>
+        <Sidebar products={this.props.products} />
+        {products.map(product => {
+          return (
+            <div key={product.id}>
+              <h1 className="product-title"> {product.name} </h1>
+              <img className="media-object" src={product.picture} />
+              <h2 className="product-price"> Price: {product.price} </h2>
+              <Link to={`/products/${product.id}`}>
+                {" "}
+                <button> {product.name} </button>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  };
+};
+
+export default connect(mapStateToProps)(AllProducts);
+
