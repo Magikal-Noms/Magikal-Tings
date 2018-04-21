ee08fb66b2f4e3d877fe0691922eb78626a8a905
Crystal Magloire <crys.magloire@gmail.com>
2018-04-21 14:54:56 -0400

    post changes

--- server/api/cart.js
+++ server/api/cart.js
@@ -6,7 +6,7 @@ module.exports = router
 
 router.get('/', (req,res,next) => {
  const id = req.user ? req.user.id : req.sessionID
- console.log('body', r3eq.body)
+ console.log('body', req.user.id)
  Order.findAll({where: {userId: id, status: "pending"}, include: [{model: LineItem, include: [Product]}]})
        .then(cart => res.json(cart))
        .catch(next)
@@ -14,5 +14,12 @@ router.get('/', (req,res,next) => {
 
 router.post('/', (req,res,next) => {
   const id = req.user ? req.user.id : req.sessionID
-  Order.findOrCreate({where: {userId: id, status: 'pending'} })
+  User.findOne({where: {id}})
+    .then(user => Order.findOrCreate({where: {
+      id, 
+      shippingAddress: user.shippingAddress,
+      billingAddress: user.billingAddress,
+      status: 'pending'
+    }}))
+  
 })
