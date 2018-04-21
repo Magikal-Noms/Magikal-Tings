425b6cb94904a7a4b71a8081182d38304469c71e
Priya <banspri0@gmail.com>
2018-04-21 14:08:52 -0400

    Switching

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const {LineItem, Order} = require('../db/models');
+
+module.exports = router;
 
 router.get('/', (req,res,next) => {
-    const id = req.user ? req.user.id : req.sessionID; 
+    const id = req.user ? req.user.id : req.sessionID;
 
     Order.findAll({where: {userId: id, status: "complete"}, include: [LineItem]})
           .then(orders => res.json(orders))
           .catch(next)
    })
-
-module.exports = router; 
