490df2888ef2797f5766534ddc64b180cd155435
Jane Costa <yberdysh@gmail.com>
2018-04-21 13:57:58 -0400

    cart page is working for users with no items in cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -7,18 +7,18 @@ import CartItem from './CartItem';
 class Cart extends Component {
   render() {
     console.log("cart", this.props.cart)
-    console.log("line items", this.props.cart[0]["line-items"][0].product)
-    const items = this.props.cart[0]["line-items"]
+    // console.log("line items", this.props.cart[0]["line-items"][0].product)
+    const items = this.props.cart.length ? this.props.cart[0]["line-items"] : null
     console.log(" items!!!", items)
     return (
       <div>
-        {items && items.map(item => {
+        {items ? items.map(item => {
           return (
             <div key={item.id}>
               <CartItem item={item}/>
             </div>
             )
-        })}
+        }) : <p>Your cart is empty. </p>}
       </div>
     );
   }
