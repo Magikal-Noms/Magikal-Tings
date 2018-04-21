f5d759c72a20c648137d7ff7394a713e960a0c29
Jane Costa <yberdysh@gmail.com>
2018-04-21 12:38:45 -0400

    backend route for cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -5,7 +5,9 @@ module.exports = router
 
 
 router.get('/', (req,res,next) => {
-  LineItem.findAll({include:[{model: Order, include: [User]}, Product]})
+  const id = req.user ? req.user.id : req.sessionID
+  console.log("req.session!!!!!!!!!!!!!!!!!", id)
+  Order.findAll({where: {userId: id, status: "pending"}, include: [LineItem]})
         .then(cart => res.json(cart))
         .catch(next)
 })
