6729710014433e90feaee2698b5fffb08c9fddd8
Jane Costa <yberdysh@gmail.com>
2018-04-22 17:18:51 -0400

    adding orders model fixed

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {LineItem, Order} = require('../db/models');
+const {LineItem, Order, Product} = require('../db/models');
 
 module.exports = router;
 
@@ -8,7 +8,7 @@ module.exports = router;
 router.get('/', (req,res,next) => {
     const id = req.user.id;
 
-    Order.findAll({where: {userId: id, status: "complete"}, include: [LineItem]})
+    Order.findAll({where: {userId: id, status: "complete"}, include: [{model: LineItem, include: [Product]}]})
           .then(orders => res.json(orders))
           .catch(next)
    })
@@ -16,5 +16,5 @@ router.get('/', (req,res,next) => {
 router.get('/:orderId', (req, res, next) => {
     Order.findById(+req.params.orderId, { include: [LineItem]})
         .then(orders => res.json(orders))
-        .catch(next); 
+        .catch(next);
 })
