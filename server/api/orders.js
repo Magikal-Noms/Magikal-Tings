50045b56b89593db060fe80f57e0a174c5a93d32
Christen Martin <christenmartin@Christens-MacBook-Pro.local>
2018-04-23 11:13:31 -0400

    fixing merge bug

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,18 +3,23 @@ const {LineItem, Order, Product} = require('../db/models');
 
 module.exports = router;
 
-//card/products/
 
 router.get('/', (req,res,next) => {
     const id = req.user.id;
 
     Order.findAll({where: {userId: id, status: "complete"}, include: [{model: LineItem, include: [Product]}]})
-          .then(orders => res.json(orders))
+          .then((orders) => {
+              if (!orders) throw new Error('You have no completed orders'); 
+              res.json(orders)
+          })
           .catch(next)
    })
 
 router.get('/:orderId', (req, res, next) => {
     Order.findById(+req.params.orderId, { include: [LineItem]})
-        .then(orders => res.json(orders))
+        .then((orders) =>{
+            if (!order) throw new Error('This order does not exist')
+            res.json(orders)
+        }) 
         .catch(next);
 })
