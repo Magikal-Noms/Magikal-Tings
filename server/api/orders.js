35f7566527ba03bf897544d5e04558a6d37c17b7
Christen Martin <christenmartin@Christens-MacBook-Pro.local>
2018-04-23 11:21:38 -0400

    fixingMergeBug

--- server/api/orders.js
+++ server/api/orders.js
@@ -8,18 +8,12 @@ router.get('/', (req,res,next) => {
     const id = req.user.id;
 
     Order.findAll({where: {userId: id, status: "complete"}, include: [{model: LineItem, include: [Product]}]})
-          .then((orders) => {
-              if (!orders) throw new Error('You have no completed orders'); 
-              res.json(orders)
-          })
+          .then(orders => res.json(orders))
           .catch(next)
    })
 
 router.get('/:orderId', (req, res, next) => {
     Order.findById(+req.params.orderId, { include: [LineItem]})
-        .then((orders) =>{
-            if (!order) throw new Error('This order does not exist')
-            res.json(orders)
-        }) 
+        .then(orders => res.json(orders)) 
         .catch(next);
 })
