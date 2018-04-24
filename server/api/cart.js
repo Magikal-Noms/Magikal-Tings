7b7b1e9dccd8b35b4f5bde76f22a1ef831abac53
Priya <banspri0@gmail.com>
2018-04-24 18:35:57 -0400

    Fixed bugs and for sure (ish) got cart to persist for both logged in and unlogged in users

--- server/api/cart.js
+++ server/api/cart.js
@@ -5,45 +5,63 @@ module.exports = router
 
 router.use( async (req,res,next) => {
 
-  let cart;
-
-  if (req.session.cartId !== undefined)
-  {
-    cart = await Order.findOne({
-      where: {
-        id: req.session.cartId,
-        status: 'pending'
-      }
-    }).catch(next)
-  }
-
+  let cart
+  const search = {status: 'pending'}
+  if (req.session.cartId)
+    search.id = req.session.cartId
   else if (req.user)
-  {
-    cart = await Order.findOne({
-      where: {
-        userId: req.user.id,
-        status: 'pending'
-      }
-    }).catch(next)
-  }
+    search.userId = req.user.id
+  console.log('!!!! ', search)
+  if (!req.user && !req.session.cartId)
+    cart = await Order.create(search)
+  else
+    cart = await Order.findOrCreate({ where: search })
+    .then(([foundOrCreatedCart]) => {
+      if (req.user && req.session.cartId && !foundOrCreatedCart.userId)
+        return foundOrCreatedCart
+          .update({userId: req.user.id})
+      return foundOrCreatedCart
+    })
+    .catch(next)
 
-  else {
-    cart = await Order.create({
-      status: 'pending'
-    }).catch(next)
-  }
 
-  // const [cart] = await Order.findOrCreate({where: {
-  //   req.session.cartId ? id: req.session.cartId : userId: req.user.id,
-  //   billingAddress: req.user ? req.user.billingAddress : ,
-  //   status: 'pending'
-  // }})
-  // .catch(next)
-  // console.log("*********************************Cart is", cart)
+  // if (req.session.cartId !== undefined)
+  // {
+  //   console.log("In first if")
+  //   console.log(req.session.cartId)
+  //   cart = await Order.findOne({
+  //     where: {
+  //       id: req.session.cartId,
+  //       status: 'pending'
+  //     }
+  //   }).catch(next)
+  // }
+  //
+  // else if (req.user)
+  // {
+  //   console.log("In second if")
+  //   cart = await Order.findOrCreate({
+  //     where: {
+  //       userId: req.user.id,
+  //       status: 'pending'
+  //     }
+  //   }).then(([createdCart]) => createdCart)
+  //     .catch(next)
+  // }
+  //
+  // else {
+  //   console.log("In else")
+  //   cart = await Order.create({
+  //     status: 'pending'
+  //   }).catch(next)
+  // }
+
+  console.log("*********************************Cart is", cart)
 
   req.cart = cart
   // console.log("After req.cart is", req.cart)
-  req.session.cartId = cart.id
+  if (req.cart)
+    req.session.cartId = cart.id
   next()
 })
 router.get('/', (req,res,next) => {
